e3ebf6c4bc388c91891693686740646869ee56a1
Sergey Makovkin <mackovckin.sergei@yandex.ru>
2020-12-12 21:48:41 +0300

    Finishing touches

--- js/script.js
+++ js/script.js
@@ -1,39 +1,10 @@
 $('.slider').slick({
     infinite: true,
     speed: 300,
+    dots: true,
     fade: true,
     cssEase: 'linear',
     arrows: true,
     slidesToShow: 1,
     slidesToScroll: 1
   });
-
-  
-window.onload = function() {
-  
-  var modal = document.getElementById("myModal");
-  
-  var btn1 = document.getElementById("signupbtn1");
-  var btn2 = document.getElementById("signupbtn2");
-  var btn3 = document.getElementById("signupbtn3");
-  var btn4 = document.getElementById("signupbtn4");
-
-  btn1.onclick = function() {
-    modal.style.display = "block";
-  }
-  btn2.onclick = function() {
-    modal.style.display = "block";
-  }
-  btn3.onclick = function() {
-    modal.style.display = "block";
-  }
-  btn4.onclick = function() {
-    modal.style.display = "block";
-  }
-
-  window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  }
-}
